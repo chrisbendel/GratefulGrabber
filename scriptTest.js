2e2f058a82c0114ae7e29997157b1f628d5d58bf
Chris Bendel <chrissbendel@gmail.com>
2016-08-27 19:02:40 -0400

    mustache templating

--- scriptTest.js
+++ scriptTest.js
@@ -9,7 +9,6 @@ function doProgress(oEvent) {
     if (newPercent > oldPercent) {
       document.getElementById("progress").innerHTML = "Compressing " + getTotalPercent() + " % ";
     }
-
   }
 
   function updatePercent(url, percent) {
@@ -25,77 +24,86 @@ function doProgress(oEvent) {
     var array = _.map(getPercents(), function(value, index) {
       return value;
     });
-
-    var sum = _.reduce(array, function(a,b){ return a + b; }, 0);
+    var sum = _.reduce(array, function(a, b) {
+      return a + b;
+    }, 0);
     return Math.floor((sum / array.length) * 100);
-
   }
-
 }
 
+function deferredAddZip(url, filename, zip) {
+  var deferred = $.Deferred();
+  JSZipUtils.getBinaryContent(url,
+    function startDownload(err, data) {
+      if (err) {
+        deferred.reject(err);
+      } else {
+        zip.file(filename, data, {
+          binary: true
+        });
+        deferred.resolve(data);
+      }
+    }, doProgress);
+  return deferred;
+}
 
-  function deferredAddZip(url, filename, zip) {
-    var deferred = $.Deferred();
-    JSZipUtils.getBinaryContent(url,
-        function startDownload(err, data) {
-          if(err) {
-            deferred.reject(err);
-            console.log("Ahh Craig ");
-          } else {
-            zip.file(filename, data, {binary:true});
-            deferred.resolve(data);
-          }}, doProgress);
-    return deferred;
+function download_all(urls) {
+  var zip = new JSZip();
+  var deferreds = [];
+
+  $.each(urls, function(k, v) {
+    var song = JSON.parse(v)
+    var url = song.sources[0].file;
+    var full_url = "https://archive.org" + url;
+
+    var title = url.substring(url.lastIndexOf('/') + 1);
+    deferreds.push(deferredAddZip(full_url, title, zip));
+  });
+
+  $.when.apply($, deferreds).done(function() {
+    var blob = zip.generate({
+      type: "blob"
+    });
+    $("#downloadAll").prop("disabled", false);
+    $("#progress").text("Compressing: Finished");
+    var url = window.location.pathname;
+    var show = url.substring(url.lastIndexOf('/') + 1);
+    saveAs(blob, show + ".zip");
+  }).fail(function(err) {
+    console.log("Ahh craig:" + err);
+  });
+};
+
+function startApp() {
+  console.log('jwloaded');
+}
+
+var req = new XMLHttpRequest();
+req.open("GET", chrome.extension.getURL('box.html'), true);
+req.onreadystatechange = function() {
+  if (req.readyState == 4 && req.status == 200) {
+    console.log('loaded');
+    var template = Mustache.to_html(req.responseText, {
+      name: "Chris"
+    });
+    $('#theatre-ia-wrap').after(template);
   }
+};
+req.send(null);
 
-  function download_all(urls)
-  {
-    var zip = new JSZip();
-    var deferreds = [];
+var player = null;
 
-    $.each(urls, function(k,v) {
-      var song = JSON.parse(v)
-      var url = song.sources[0].file;
-      var full_url = "https://archive.org" + url;
-
-      // 64kbps support not working yet...
-      // if ($(".content:contains('64kbps')").size() > 0) {
-      //  full_url = full_url.replace(".mp3","_64kb.mp3");
-      // }
-
-      var title = url.substring(url.lastIndexOf('/')+1);
-      deferreds.push(deferredAddZip(full_url, title, zip));
-    });
-
-    $.when.apply($, deferreds).done(function () {
-      var blob = zip.generate({type:"blob"});
-      $("#downloadAll").prop("disabled",false);
-      $("#progress").text("Compressing: Finished");
-      var url = window.location.pathname;
-      var show = url.substring(url.lastIndexOf('/')+1);
-      saveAs(blob, show + ".zip");
-
-    }).fail(function (err) {
-      console.log("Ahh craig:" + err);
-    });
-
-  };
-
-
-if ($('#wrap').length) {
-
-var myBox = $('.thats-right').prepend('<div class="boxy" id="downloadBox" style="height:auto; text-align: center;" />');
 var myContent = $('<p />');
 var myList = [];
-var numOfSongs = jwplayer('jw6').getPlaylist();
+var numOfSongs = jwplayer('jw6').getPlaylist().length;
 
-for (var i = 0; i < numOfSongs.length; i++) {
-    var song = jwplayer('jw6').getPlaylist()[i].file;
-    var title = jwplayer('jw6').getPlaylist()[i].title;
-    var newUrl = "http://www.archive.org".concat(song);
-    myList[i] = newUrl;
-    myContent.append("<a href=\"" + newUrl + "\" class=\"downloadfile\"  download>" + title + "</a>");
-    myContent.append("<br/>");
+for (var i = 0; i < numOfSongs; i++) {
+  var song = jwplayer('jw6').getPlaylist()[i].file;
+  var title = jwplayer('jw6').getPlaylist()[i].title;
+  var newUrl = "http://www.archive.org".concat(song);
+  myList[i] = newUrl;
+  myContent.append("<a href=\"" + newUrl + "\" class=\"downloadfile\"  download>" + title + "</a>");
+  myContent.append("<br/>");
 }
 
 $('#downloadBox').append("<h2 style=\"text-align:center;\">Grateful Grabber</h2> <br/> <p style=\"text-align:center\"> Download This Show!</p>");
@@ -103,19 +111,16 @@ $('#downloadBox').append("<input id=\"downloadAll\" style =\"width:125px; font-w
 $('#downloadBox').append("<input id=\"downloadIndividually\" style =\"font-weight: bold; width:125px;; height: auto; white-space:normal; border-radius:7px;\" type=\"button\" value=\"Individual Songs (No Folder)\" /> <hr/>");
 $('#downloadBox').append(myContent);
 
-
-
-$('#downloadAll').click(function(){
-    $("#downloadAll").prop("disabled",true);
-    if ($("#progress").length == 0) {
-        $("<div id='progress'></div>").insertAfter("#downloadAll");
-    }
-    download_all(myList);
+$('#downloadAll').click(function() {
+  $("#downloadAll").prop("disabled", true);
+  if ($("#progress").length == 0) {
+    $("<div id='progress'></div>").insertAfter("#downloadAll");
+  }
+  download_all(myList);
 });
 
-$('#downloadIndividually').click(function(){
-    for (i = 0; i < numOfSongs.length; i++) {
+$('#downloadIndividually').click(function() {
+  for (i = 0; i < numOfSongs.length; i++) {
     $('.downloadfile')[i].click();
-    };
+  };
 });
-}
