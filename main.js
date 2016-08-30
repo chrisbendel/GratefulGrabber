32c987b29ed7760eced4e68a36811233ea66f1a9
Chris Bendel <chrissbendel@gmail.com>
2016-08-30 01:43:03 -0400

    loading bar + new layout

--- main.js
+++ main.js
@@ -4,12 +4,19 @@ function resetMessage() {
   $("#result")
     .removeClass()
     .text("");
+  // $("#circle img").remove();
 }
 
 function Message(text) {
   resetMessage();
   $("#result")
     .addClass("alert alert-success")
+    .text(text);
+}
+
+function Loading(text) {
+  $("#result")
+    .addClass("alert alert-warning")
     .text(text);
 }
 
@@ -20,33 +27,34 @@ function Error(text) {
     .text(text);
 }
 
-function updatePercent(percent) {
-  $("#progressBar").removeClass("hide")
-    .find(".progress-bar")
-    .attr("aria-valuenow", percent)
-    .css({
-      width: percent + "%"
-    });
-}
+var songs = [],
+  object = [];
 
-var urls = [],
-  songs = [],
-  object = [],
-  songTitles = [];
+var totalSize = 0,
+  fileSize;
 
 var list = $('#wrap').find('.container:last').find('script:last').html();
 var myList = list.match(/\{\"title\"(.*?\}\]\})/g);
 _.each(myList, function(details) {
   details = JSON.parse(details);
-  var url = "https://www.archive.org".concat(details.sources[0].file);
+  var url = "https://www.archive.org" + details.sources[0].file;
   trackLink = "<a href=\"" + url + "\" class=\"downloadfile\"  download>" + details.title + "</a>";
+
+  var req = $.ajax({
+    type: "HEAD",
+    url: url,
+    success: function() {
+      fileSize = req.getResponseHeader('Content-Length');
+      fileSize = parseFloat(fileSize);
+      totalSize += fileSize;
+    }
+  });
   object.push(details);
-  songTitles.push(details.title);
   songs.push(trackLink);
-  urls.push(url);
 });
 
 var req = new XMLHttpRequest();
+var owl = chrome.extension.getURL('icons/owl.png');
 req.open("GET", chrome.extension.getURL('box.html'), true);
 req.onreadystatechange = function() {
   if (req.readyState == 4 && req.status == 200) {
@@ -56,12 +64,12 @@ req.onreadystatechange = function() {
     });
     var template = Mustache.to_html(req.responseText, {
       songs: songs,
+      owl: owl
     });
     $('#theatre-ia-wrap').after(template);
   }
 };
 req.send(null);
-
 
 function deferredAddZip(url, filename, zip) {
   var deferred = $.Deferred();
@@ -81,6 +89,20 @@ function deferredAddZip(url, filename, zip) {
 
 $('body').on('click', '#downloadAll', function() {
   resetMessage();
+  Loading('Downloading the show now. Hang tight for a sec.');
+
+  $('#circle').circleProgress({
+    value: .01,
+    size: 200,
+    animation: {
+      duration: 250,
+      easing: "circleProgressEasing"
+    },
+    fill: {
+      gradient: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+    }
+  });
+
   var zip = new JSZip();
   var deferreds = [];
   var showName = $(".key-val-big:contains('Published')").find('a:first').text();
@@ -97,7 +119,7 @@ $('body').on('click', '#downloadAll', function() {
     });
 
     saveAs(blob, showName + ".zip");
-    Message("done !");
+    Message("Your download has finished!");
   }).fail(function(err) {
     Error(err);
   });
@@ -106,29 +128,26 @@ $('body').on('click', '#downloadAll', function() {
 function doProgress(oEvent) {
   if (oEvent.lengthComputable) {
     var percentComplete = oEvent.loaded / oEvent.total;
-    var oldPercent = getTotalPercent();
     updatePercent(oEvent.target.responseURL, percentComplete);
-    var newPercent = getTotalPercent();
-    if (newPercent > oldPercent) {
-      $('#progressBar').css('width', getTotalPercent() + '%').attr('aria-valuenow', getTotalPercent());
-    }
+    var percent = getTotalPercent() / object.length;
+    $('#circle').circleProgress('value', percent);
   }
+}
 
-  function updatePercent(url, percent) {
-    xhr[url] = percent;
-  }
+function updatePercent(url, percent) {
+  xhr[url] = percent;
+}
 
-  function getPercents() {
-    return xhr;
-  }
+function getPercents() {
+  return xhr;
+}
 
-  function getTotalPercent() {
-    var array = _.map(getPercents(), function(value, index) {
-      return value;
-    });
-    var sum = _.reduce(array, function(a, b) {
-      return a + b;
-    }, 0);
-    return Math.floor((sum / array.length) * 100);
-  }
+function getTotalPercent() {
+  var array = _.map(getPercents(), function(value, index) {
+    return value;
+  });
+  var sum = _.reduce(array, function(a, b) {
+    return a + b;
+  }, 0);
+  return sum;
 }
